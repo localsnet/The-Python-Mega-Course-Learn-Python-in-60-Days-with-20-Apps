98daeaf04da748d8aa5519a919fb37014f5f7179
localsnet <localsnet@example.com>
2023-05-07 20:52:37 -0600

    Day17  Exercices completed

---
+++ day17/e1.py
@@ -0,0 +1,24 @@
+import PySimpleGUI as sg
+from day14.convert13 import convert
+
+
+label1 = sg.Text("Enter feet: ")
+input1 = sg.Input(key="feet")
+label2 = sg.Text("Enter inches: ")
+input2 = sg.Input(key="inches")
+convert_button = sg.Button("Convert")
+output_label = sg.Text(key='output', text_color="orange")
+
+window = sg.Window("Measure Converter",
+                   layout=[[label1, input1],
+                           [label2, input2],
+                           [convert_button, output_label]])
+
+while True:
+    event, values = window.read()
+    feet = int(values['feet'])
+    inches = int(values['inches'])
+    converted = convert(feet, inches)
+    window['output'].update(value= f"{converted} m")
+
+window.close()
