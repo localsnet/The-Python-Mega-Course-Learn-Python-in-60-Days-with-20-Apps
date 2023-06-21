3f3f2c1ad68b9719e5a06291285bffcb96512ca1
localsnet <localsnet@example.com>
2023-06-20 18:56:04 -0600

    rename from main to Home

--- app2-portfolio/Home.py
+++ app2-portfolio/Home.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas
 
+
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
@@ -17,11 +18,19 @@ with col2:
 content2 = """Наш позывной .-..--..-.-.--- . Copyright © 2023 Free Software Foundation, Inc.  License GPLv3+"""
 st.text(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 with col3:
-    for index, row in df.iterrows():
+    for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/"+ row["image"])
+        st.write(f"[Source code]({row['url']})")
 
-
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
