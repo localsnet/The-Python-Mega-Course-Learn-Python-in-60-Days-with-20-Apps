2af78a790b2ad5d4f4e622e6990b341bc6ce0c21
localsnet <localsnet@example.com>
2023-06-20 19:46:55 -0600

    Revert "updated for pdf"
    This reverts commit a4486ff128edb2fe88af98acf6adc33fd439c94d.

--- app3-pdf-template/main.py
+++ app3-pdf-template/main.py
@@ -6,7 +6,6 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 df = pd.read_csv("topics.csv")
 
-
 for index, row in df.iterrows():
     pdf.add_page()
 
@@ -14,9 +13,6 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10,21, 200, 21)
-
-    for i in range(row["Pages"] -1):
-        pdf.add_page()
 
 
 pdf.output("output.pdf")
