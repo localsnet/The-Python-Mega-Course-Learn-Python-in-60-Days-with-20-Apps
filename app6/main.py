e736546862de0be707baae53f8d75dc52e03a3b9
localsnet <localsnet@example.com>
2024-09-05 16:27:19 -0600

    day31: URL endpoints in work, beginning

--- app6/main.py
+++ app6/main.py
@@ -27,5 +27,13 @@ def all_data(station):
     result = df.to_dict(orient='records')
     return result
 
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df[df["    DATE"].str.startswith(str(year))]
+    return result
+
+
 if __name__ == "__main__":
     app.run(debug=True)
