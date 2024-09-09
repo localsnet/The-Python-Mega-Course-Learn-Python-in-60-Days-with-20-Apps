42e3cbb56ecc593b59c1d8702aa706397277e21e
localsnet <localsnet@gmail.com>
2024-09-09 13:07:10 -0600

    Day 31: Added url endpoints yearly

--- app6/main.py
+++ app6/main.py
@@ -30,8 +30,9 @@ def all_data(station):
 @app.route("/api/v1/yearly/<station>/<year>")
 def yearly(station, year):
     filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
-    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
-    result = df[df["    DATE"].str.startswith(str(year))]
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
     return result
 
 
